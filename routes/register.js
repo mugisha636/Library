77f3e6373e5a231aea5099534506ff2558f6c18b
mugisha636 <mugisha134emmy@gmail.com>
2023-03-08 08:40:53 +0200

    adding start

--- routes/register.js
+++ routes/register.js
@@ -11,10 +11,10 @@ const router=express.Router()
 router.post('/create',AuthValidation.verifySignup, register.registerUser )
 router.post('/login',register.login)
 
-router.get('/users', register.getUsers)
-router.get('/users/:userId', register.oneUser)
-router.delete('/users/:userId', register.deleteUser)
-router.put('/users/:userId', register.updateUser)
+router.get('/users',register.getUsers)
+router.get('/users/:userId',register.oneUser)
+router.delete('/users/:userId',register.deleteUser)
+router.put('/users/:userId',register.updateUser)
 
 
 
